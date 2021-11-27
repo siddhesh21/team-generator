cdea72555b5dea7ec38bb78bc2612055cddd0dbc
Siddhesh Kankekar <kankekas@tcd.ie>
2021-11-27 19:00:02 +0000

    component design completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'team-app';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
+
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty";
+      return;
+    }
+
+    this.errorMessage = '';
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
 }
